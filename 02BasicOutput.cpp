1750dd7489c45846253ba6b143f33a12b6a68002
cessryy <icatlosarry2@gmail.com>
2024-10-03 08:25:47 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Our Lady of Lourdes College Foundation\n";
+	cout << "College of Computer Studies\n";
+	cout << "BSIT-1B\n";
+	cout << "Prencis Sarry V. Icatlo\n";
+	cout << "P-7 Core Shelter, Calangcawan Norte, Vinzons\n";
+	return 0;
+}
